eda42a951f2459e74d6815a915b68152149818fc
Broască Ioan Iulian <iulianbroasca@yahoo.ro>
2020-01-16 00:21:00 +0200

    Created REST API, to optimize the proccess

---
+++ API/AI/AI/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace AI.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
---
+++ API/AI/AI/Controllers/RoomsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace AI.Controllers
+{
+    public class RoomsController : ApiController
+    {
+        string json;
+        public string Get()
+        {
+            return json;
+        }
+
+        [Route("rooms/all")]
+        [HttpPost]
+        public async Task<string> PostRawBufferManual()
+        {
+            string result = await Request.Content.ReadAsStringAsync();
+
+            return result;
+        }
+
+        //[HttpPost]
+        //public IHttpActionResult All([FromBody] string s)
+        //{
+        //    List<string> movies = new List<string>();
+        //    movies.Add("okok");
+        //    movies.Add("okok1");
+        //    movies.Add("okok2");
+        //    movies.Add(s);
+        //    return Ok(movies);
+        //}
+    }
+}
